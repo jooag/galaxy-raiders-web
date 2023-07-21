325666e52103746695ab6c214fb04aeb1a051c3a
João Gabriel Josephik <joao.gabrielaaj@gmail.com>
2023-07-21 19:11:28 -0300

    Adds leaderboard and styles.
    Co-authored-by: Fernando-crz <fernando.cruz.ctt@gmail.com>

--- pages/index.vue
+++ pages/index.vue
@@ -1,21 +1,114 @@
-<template>
+<template> 
   <div>
-    <header>
-      <nav>
+  <div class='title'>
+  <h1> Galaxy Raiders</h1>
+  </div>
+  <div class='menu'>   
         <ul>
           <li> <NuxtLink to="/game"> Iniciar Partida </NuxtLink> </li>
           <li> <NuxtLink to="/leaderboard"> Placar </NuxtLink> </li>
-        </ul>
-      </nav>
+          <li> <a v-on:click='exit()' href='#'> Sair </a> </li>
 
-    </header>
+        </ul>
+    
 
   </div>
-
+  </div>
 </template>
 
 
 <style>
 
+d1 {
+  align-self:center;
+  font-weight: bold;
+}
 
+ul {
+  list-style-type: none;
+  padding: 0;
+  display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
+  align-self:center;
+  width: 100%;
+}
+li {
+  margin: 0 0.5rem;
+  padding: 0.25rem;
+  font-size: 1.2rem;
+  color:azure;
+  text-align: center;
+  width: 100%;
+}
+
+nav {
+  padding: 0 1rem;
+}
+
+a,
+a:visited {
+  text-decoration: none;
+  color: inherit;
+  text-align: center;
+
+}
+
+a:hover {
+  color: #00c58e;
+}
+/* home route and active route will show in bold as it matches / and /about */
+a.nuxt-link-active {
+  font-weight: bold;
+}
+/* exact link will show the primary color for only the exact matching link */
+a.nuxt-link-exact-active {
+  color: #00c58e;
+}
+
+body {
+  font-family: system-ui, -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
+    Helvetica Neue, Arial, Noto Sans, sans-serif, Apple Color Emoji,
+    Segoe UI Emoji, Segoe UI Symbol, Noto Color Emoji;
+    background-color: rgb(202, 202, 202);
+  margin: 0;
+}
+
+.menu {
+  margin: 5vw;
+  margin-top: 1vw;
+  display:flex;
+  flex-direction: row;
+  align-items: center;
+  width: 90vw;
+  border-radius:1em;
+  background-color: rgb(82, 82, 82);
+}
+  .title{
+        text-align: center;
+    }
 </style>
+
+<script>
+  export default{
+
+    methods:{
+      exit: ()=>{
+         useFetch('exit', {
+      baseURL: 'http://localhost:5000',
+      method: "GET",
+      initialCache: false,
+      
+      });
+    window.location.replace("https://www.youtube.com/watch?v=dQw4w9WgXcQ");
+
+
+      }
+    }
+
+    
+  }
+  onMounted(() => {window.onpopstate = function () {
+    location.reload()
+  };})
+</script>
---
+++ pages/leaderboard.vue
@@ -0,0 +1,67 @@
+<template>
+<div>
+<div class = 'title'>
+    <h1> Placar </h1>
+</div>
+    <table>
+        <thead>
+            <tr>
+                <td>Colocação: </td>
+                <td>Pontuação: </td>
+                <td>Data: </td>
+            </tr>
+        </thead>
+        <tbody>
+            <tr v-for="(data, index) in $options.leaderboard">
+                <td>Top {{index+1}}</td>
+                <td> {{parseFloat(data.score).toFixed(2)}}</td>
+                <td>{{toDateTime(data.time)}}</td>
+            </tr>
+        </tbody>
+    </table>
+    <ul>
+        <li class='dark'><NuxtLink to="/"> Voltar </NuxtLink> </li>
+    </ul>
+</div>
+</template>
+
+
+<style>
+.dark{
+    color:#272727;
+}
+    .title{
+        text-align: center;
+    }
+  table {
+        width: 100%;
+        border: 1px solid #ddd;
+        }
+
+        td {
+        padding: 16px;
+        }
+
+        tr:nth-child(even) {
+        background-color: #eee;
+        }
+</style>
+<script>
+      import json from '~/assets/Leaderboard.json'
+      export default{
+          methods: {
+              toDateTime: (secs) => {
+                var d = new Date(1970, 0, 1);
+                d.setSeconds(secs/1000);
+                var datestring = d.getDate()  + "-" + (d.getMonth()+1) + "-" + d.getFullYear() + " " +
+d.getHours() + ":" + d.getMinutes();
+                return datestring;
+          }
+          },
+        leaderboard: json,
+        }
+      
+
+    
+    
+</script>
